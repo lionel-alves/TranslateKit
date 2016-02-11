c46866c07f72649fdda186b1493f702e560139c6
Lionel Alves <alves.lionel5@gmail.com>
2016-02-10 22:52:49 -0500

    Add `Alamofire` dependancy using Carthage Add `Defintion` Model Add `Client` and the `translate` method that get the definitions of a word from urban dictionary api Add tests for `translate`

---
+++ TranslateKit/TranslateKit.h
@@ -0,0 +1,19 @@
+//
+//  TranslateKit.h
+//  TranslateKit
+//
+//  Created by Lionel on 1/23/16.
+//  Copyright © 2016 weTranslate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TranslateKit.
+FOUNDATION_EXPORT double TranslateKitVersionNumber;
+
+//! Project version string for TranslateKit.
+FOUNDATION_EXPORT const unsigned char TranslateKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TranslateKit/PublicHeader.h>
+
+
