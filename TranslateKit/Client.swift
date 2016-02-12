7bfd02a050fd3693221b322ec7f29d707213e6b1
Lionel Alves <alves.lionel5@gmail.com>
2016-02-12 00:01:03 -0500

    Remove Alamofire dependancy

--- TranslateKit/Client.swift
+++ TranslateKit/Client.swift
@@ -7,25 +7,60 @@
 //
 
 import Foundation
-import Alamofire
 
 public typealias JSONDictionary = [String: AnyObject]
 
 public class Client {
 
+    public let URLSession: NSURLSession
     public let baseURL = "http://api.urbandictionary.com/v0"
 
-    public func define(word word: String, completion: [Definition]? -> Void) {
-        Alamofire.request(.GET, "\(baseURL)/define", parameters: ["term": word])
-            .responseJSON { response in
+    public init(URLSession: NSURLSession = defaultSession) {
+        self.URLSession = URLSession
+    }
 
-                if let JSON = response.result.value as? JSONDictionary,
-                    list = JSON["list"] as? [JSONDictionary] {
-                        let definitions = list.flatMap { Definition(dictionary: $0) }
-                        completion(definitions)
-                } else {
-                    completion(nil)
-                }
+    static let defaultSession: NSURLSession = {
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        return NSURLSession(configuration: configuration)
+    }()
+
+
+    // MARK: - API
+
+    public func define(word word: String, completion: [Definition]? -> Void) {
+
+        guard let URL = NSURL(string: "\(baseURL)/define?term=\(word)") else {
+            completion(nil)
+            return
         }
+
+        let request = NSMutableURLRequest(URL: URL)
+
+        performRequest(request) { (dictionary: JSONDictionary?) -> Void in
+            if let dictionary = dictionary, list = dictionary["list"] as? [JSONDictionary] {
+                let definitions = list.flatMap { Definition(dictionary: $0) }
+                completion(definitions)
+            }
+        }
+    }
+
+
+    // MARK: - Private
+
+    private func performRequest(URLRequest: NSURLRequest, completion: JSONDictionary? -> Void) -> NSURLSessionDataTask? {
+
+        let task = URLSession.dataTaskWithRequest(URLRequest) { data, response, error in
+            guard let data = data,
+                parseData = try? NSJSONSerialization.JSONObjectWithData(data, options: []),
+                json = parseData as? JSONDictionary else {
+                    completion(nil)
+                    return
+            }
+
+            completion(json)
+        }
+
+        task.resume()
+        return task
     }
 }
--- TranslateKitTests/TranslateKitTests.swift
+++ TranslateKitTests/TranslateKitTests.swift
@@ -7,7 +7,6 @@
 //
 
 import XCTest
-import Alamofire
 @testable import TranslateKit
 
 class TranslateKitTests: XCTestCase {
