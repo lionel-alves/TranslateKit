fd28c7e091189322f264ef36b4aa939c644e74a1
Lionel Alves <me@lionelalves.com>
2016-03-10 23:35:13 -0500

    * Fix Translation initialization from dictionary

--- TranslateKit/Translation.swift
+++ TranslateKit/Translation.swift
@@ -9,20 +9,20 @@
 import UIKit
 
 public struct Translation: DictionaryDeserializable, DictionarySerializable {
-    
+
     public let meanings: [Meaning]
     public let additionalMeanings: [Meaning]
     public let compoundMeanings: [Meaning]
-    
+
     public init?(dictionary:JSONDictionary) {
-        guard let principalesDictionary = dictionary["PrincipalMeanings"] as? JSONDictionary else { return nil }
-        
-        let meanings = Translation.meanings(fromDictionary: principalesDictionary)
+        guard let principalesDictionary = dictionary["PrincipalMeanings"] as? [JSONDictionary] else { return nil }
+
+        let meanings = principalesDictionary.flatMap { Meaning(dictionary: $0) }
         guard meanings.count > 0 else { return nil }
-        
+
         self.meanings = meanings
-        self.additionalMeanings = Translation.meanings(fromDictionary: dictionary["AdditionalMeanings"] as? JSONDictionary)
-        self.compoundMeanings = Translation.meanings(fromDictionary: dictionary["CompoundMeanings"] as? JSONDictionary)
+        self.additionalMeanings = (dictionary["AdditionalMeanings"] as? [JSONDictionary])?.flatMap { Meaning(dictionary: $0) } ?? []
+        self.compoundMeanings = (dictionary["CompoundMeanings"] as? [JSONDictionary])?.flatMap { Meaning(dictionary: $0) } ?? []
     }
 
     public init?(webserviceDictionary:JSONDictionary) {
@@ -37,18 +37,6 @@ public struct Translation: DictionaryDeserializable, DictionarySerializable {
     }
 
     // MARK: - Private
-    
-    static private func meanings(fromDictionary dictionary: JSONDictionary?) -> [Meaning] {
-        guard let meaningsDictionary = dictionary else { return [] }
-        
-        let sortedDictionary = meaningsDictionary.sort({ $0.0 < $1.0 })
-        let meanings:[Meaning] = sortedDictionary.flatMap {
-            guard let dictionary = $0.1 as? JSONDictionary else { return nil }
-            return Meaning(dictionary: dictionary)
-        }
-        
-        return meanings
-    }
 
     static private func meanings(fromWebDictionary dictionary: JSONDictionary?) -> [Meaning] {
         guard let meaningsDictionary = dictionary else { return [] }
@@ -63,11 +51,11 @@ public struct Translation: DictionaryDeserializable, DictionarySerializable {
     }
 
     public var dictionary: JSONDictionary {
-        
+
         let meaningsDictionary = meanings.map { $0.dictionary }
         let additionalMeaningsDictionary = additionalMeanings.map { $0.dictionary }
         let compoundMeaningsDictionary = compoundMeanings.map { $0.dictionary }
-        
+
         return [
             "PrincipalMeanings" : meaningsDictionary,
             "AdditionalMeanings" : additionalMeaningsDictionary,
